3706664716509f03de60e758c5783fd6374a004c
zwodev <zwodev@gmail.com>
2023-09-16 21:35:37 +0200

    vm1-test runs with two displays.

--- python/tests/vm1-test.py
+++ python/tests/vm1-test.py
@@ -4,168 +4,120 @@ import gi
 import logging
 import os
 import platform
-
+import pykms
 
 gi.require_version('GLib', '2.0')
 gi.require_version('GObject', '2.0')
 gi.require_version('Gst', '1.0')
 
-
 from gi.repository import Gst, GObject, GLib
-
 
 logging.basicConfig(level=logging.DEBUG, format="[%(name)s] [%(levelname)8s] - %(message)s")
 logger = logging.getLogger(__name__)
 
 class VMOneContainer:
-    def __init__(self):
+    def __init__(self):        
+        self.card = pykms.Card()
+        self.res = pykms.ResourceManager(self.card)
+
+        self.fd = self.card.fd
+        print("File Descriptor: ", self.fd)
+        
+        # Create plane for first screen
+        self.conn0 = self.res.reserve_connector("HDMI-A-1")
+        self.crtc0 = self.res.reserve_crtc(self.conn0)
+        self.plane0 = self.res.reserve_overlay_plane(self.crtc0)
+        print("HDMI-A-1 Connector ID: ", self.conn0.id)
+
+        # Create plane for second screen
+        self.conn1 = self.res.reserve_connector("HDMI-A-2")
+        self.crtc1 = self.res.reserve_crtc(self.conn1)
+        self.plane1 = self.res.reserve_overlay_plane(self.crtc1)
+        print("HDMI-A-1 Connector ID: ", self.conn1.id)
+
+        # Initialize Gst and create pipeline
         Gst.init(sys.argv[1:])
-        self.pipeline = Gst.Pipeline.new("test-pipeline")
+        print("Gst Version: %s", Gst.version())
+        self.pipeline = Gst.Pipeline.new("main-pipeline")
+
         self.mainloop = GLib.MainLoop()
+
         # Create bus and connect several handlers
         self.bus = self.pipeline.get_bus()
         self.bus.add_signal_watch()
-        self.bus.connect('message::eos', self.on_eos)
-        #self.bus.connect('message::tag', self.on_tag)
-        self.bus.connect('message::error', self.on_error)
-        self.elements = []
+        self.bus.connect('message::eos', self.onEos)
+        #self.bus.connect('message::tag', self.onTag)
+        self.bus.connect('message::error', self.onError)
     
-    def on_eos(self, bus, msg):
+    def onEos(self, bus, msg):
         print ('on_eos')
         self.mainloop.quit()
         self.pipeline.set_state(Gst.State.NULL)
 			
-    def on_tag(self, bus, msg):
-        pass
+    def onTag(self, bus, msg):
         taglist = msg.parse_tag()
         print ('on_tag:')
         for key in taglist.keys():
             print ('\t%s = %s' % (key, taglist[key]))
 					
-    def on_error(self, bus, msg):
+    def onError(self, bus, msg):
         error = msg.parse_error()
-        print ('on_error:', error[1])
+        print ('onError:', error[1])
         self.mainloop.quit()
         self.pipeline.set_state(Gst.State.NULL)
 
-    def addVideoElement(self, videoElement):
+    def addVideoElement0(self, videoElement):
+        videoElement.addDrmInfo(self.fd, self.plane0, self.conn0)
+        videoElement.addToPipeline(self)
+
+    def addVideoElement1(self, videoElement):
+        videoElement.addDrmInfo(self.fd, self.plane1, self.conn1)
         videoElement.addToPipeline(self)
 
     def start(self):
         self.pipeline.set_state(Gst.State.PLAYING)
         self.mainloop.run()
 
-
 class VideoElement:
     def __init__(self, file):
-        self.source = Gst.ElementFactory.make("filesrc", "source")
+        self.source = Gst.ElementFactory.make("videotestsrc")
+        self.source = Gst.ElementFactory.make("filesrc")
         self.source.set_property('location', file)
-        self.decode = Gst.ElementFactory.make("decodebin", "decode")
+        self.decode = Gst.ElementFactory.make("decodebin")
         self.sink = None
         self.kmssink_fd = None
-        if platform.machine == "aarch64":
-            self.sink = Gst.ElementFactory.make("kmssink", "sink")
-            self.kmssink_fd = self.sink.get_property("fd")
-            self.kmssink_plane_id = self.sink.get_property("plane-id")
+        if platform.machine() == "aarch64":
+            self.sink = Gst.ElementFactory.make("kmssink")
+            self.sink.set_property("skip-vsync", "true")
         else:
-            self.sink = Gst.ElementFactory.make("autovideosink", "sink")
+            self.sink = Gst.ElementFactory.make("autovideosink")
 
-    def on_new_decoded_pad(self, dbin, pad):
-        print("New Pad")
+    def onPadAdded(self, dbin, pad):
         decode = pad.get_parent()
         pipeline = decode.get_parent()
-        sink = pipeline.get_by_name('sink')
-        decode.link(sink)
+        decode.link(self.sink)
 
+    def addDrmInfo(self, fd, plane, conn):
+        self.sink.set_property("fd", fd)
+        self.sink.set_property("connector-id", conn.id)
+        self.sink.set_property("plane-id", plane.id)
+        
     def addToPipeline(self, container):
         container.pipeline.add(self.source)
         container.pipeline.add(self.decode)
         container.pipeline.add(self.sink)
-        if not self.source.link(self.decode):
+
+        if not self.source.link(self.decode) :
             logger.error("Link Error: source -> decode")
 
-        self.decode.connect("pad-added", self.on_new_decoded_pad)
+        self.decode.connect("pad-added", self.onPadAdded)
 
 def main():
     vmOne = VMOneContainer()
+    videoElement0 = VideoElement("./videos/BlenderReel_1080p.mp4")
     videoElement1 = VideoElement("./videos/BlenderReel_1080p.mp4")
-    videoElement2 = VideoElement("./videos/BlenderReel_1080p.mp4")
-    vmOne.addVideoElement(videoElement1)
-    vmOne.addVideoElement(videoElement2)
+    vmOne.addVideoElement0(videoElement0)
+    vmOne.addVideoElement1(videoElement1)
     vmOne.start()
-
-def on_new_decoded_pad(dbin, pad):
-    decode = pad.get_parent()
-    pipeline = decode.get_parent()
-    sink = pipeline.get_by_name('sink')
-    decode.link(sink)
-    pipeline.set_state(Gst.State.PLAYING)
-    
-
-def main_old():
-    # get and print the current working directory
-    current_working_directory = os.getcwd()
-    print(current_working_directory)
-
-    # initialize GStreamer
-    Gst.init(sys.argv[1:])
-
-    # create pipeline elements
-    source = Gst.ElementFactory.make("filesrc", "source")
-    source.set_property('location', './videos/BlenderReel_1080p.mp4')
-    decode = Gst.ElementFactory.make("decodebin", "decode")
-    sink = None
-
-    # create kmssink only on Raspberry Pi
-    kmssink_fd = None
-    kmssink_plane_id = None
-    if platform.machine == "aarch64":
-        sink = Gst.ElementFactory.make("kmssink", "sink")
-        kmssink_fd = sink.get_property("fd")
-        kmssink_plane_id = sink.get_property("plane-id")
-    else:
-        sink = Gst.ElementFactory.make("autovideosink", "sink")
-
-    # create empty pipeline
-    pipeline = Gst.Pipeline.new("test-pipeline")
-
-    if not pipeline or not source or not sink or not decode :
-        logger.error("Not all elements could be created")
-        sys.exit(1)
-
-
-    # Build the pipeline
-    pipeline.add(source)
-    pipeline.add(decode)
-    pipeline.add(sink)
-
-    if not source.link(decode):
-        logger.error("Link Error: source -> decode")
-        sys.exit(1)
-
-    decode.connect("pad-added", on_new_decoded_pad)
-    ret = pipeline.set_state(Gst.State.PAUSED)
-
-    if ret == Gst.StateChangeReturn.FAILURE:
-        logger.error("Unable to set the pipeline to the playing state.")
-        sys.exit(1)
-
-
-    bus = pipeline.get_bus()
-    msg = bus.timed_pop_filtered(Gst.CLOCK_TIME_NONE, Gst.MessageType.ERROR | Gst.MessageType.EOS)
-
-    # Parse message
-    if msg:
-        if msg.type == Gst.MessageType.ERROR:
-            err, debug_info = msg.parse_error()
-            logger.error(f"Error received from element {msg.src.get_name()}: {err.message}")
-            logger.error(f"Debugging information: {debug_info if debug_info else 'none'}")
-        elif msg.type == Gst.MessageType.EOS:
-            logger.info("End-Of-Stream reached.")
-        else:
-            # This should not happen as we only asked for ERRORs and EOS
-            logger.error("Unexpected message received.")
-
-    pipeline.set_state(Gst.State.NULL)
 
 main()
