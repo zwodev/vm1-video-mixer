6818b6d908439c73588952f56258d19ce6f84b65
zwodev <zwodev@gmail.com>
2023-10-05 16:55:57 +0200

    Added HdmiElement (untested).

--- python/tests/vm1-test.py
+++ python/tests/vm1-test.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 import sys
-import gi
+import gi   
 import logging
 import os
 import platform
@@ -66,21 +66,87 @@ class VMOneContainer:
         self.mainloop.quit()
         self.pipeline.set_state(Gst.State.NULL)
 
-    def addVideoElement0(self, videoElement):
-        videoElement.addDrmInfo(self.fd, self.plane0, self.conn0)
-        videoElement.addToPipeline(self)
+    # Element 0 will be displayed on HDMI0
+    def addElement0(self, element):
+        element.addDrmInfo(self.fd, self.plane0, self.conn0)
+        element.addToPipeline(self)
 
-    def addVideoElement1(self, videoElement):
-        videoElement.addDrmInfo(self.fd, self.plane1, self.conn1)
-        videoElement.addToPipeline(self)
+    # Element 1 will be displayed on HDMI1
+    def addElement1(self, element):
+        element.addDrmInfo(self.fd, self.plane1, self.conn1)
+        element.addToPipeline(self)
 
     def start(self):
         self.pipeline.set_state(Gst.State.PLAYING)
         self.mainloop.run()
 
-class VideoElement:
+class BaseElement:
+    def __init__(self):
+        self.sink = None
+        self.kmssink_fd = None
+        if platform.machine() == "aarch64":
+            self.sink = Gst.ElementFactory.make("kmssink")
+            self.sink.set_property("skip-vsync", "true")
+        else:
+            self.sink = Gst.ElementFactory.make("autovideosink")
+
+    def addDrmInfo(self, fd, plane, conn):
+        self.sink.set_property("fd", fd)
+        self.sink.set_property("connector-id", conn.id)
+        self.sink.set_property("plane-id", plane.id)
+
+    def addToPipeline(self, container):
+        pass
+
+class HdmiElement(BaseElement):
+    def __init__(self):
+        super().__init__()
+        self.source = Gst.ElementFactory.make("v4l2src")
+        
+        caps = Gst.Caps("video/x-raw,framerate=30/1,colorimetry=bt601")
+        self.filter = Gst.ElementFactory.make("capsfilter")
+        self.filter.set_property("caps", caps)
+        
+        self.parse = Gst.ElementFactory.make("rawvideoparse")
+        self.parse.set_property("format", "uyvy")
+        self.parse.set_property("width", 1920)
+        self.parse.set_property("height", 1080)
+        self.parse.set_property("framerate", 30/1)
+        
+        self.convert = Gst.ElementFactory.make("v4l2convert")
+        self.queue = Gst.ElementFactory.make("queue")
+
+    def addToPipeline(self, container):
+        container.pipeline.add(self.source)
+        container.pipeline.add(self.filter)
+        container.pipeline.add(self.parse)
+        container.pipeline.add(self.convert)
+        container.pipeline.add(self.queue)
+        container.pipeline.add(self.sink)
+
+        if not self.source.source(self.filter):
+            logger.error("Link Error: source -> filter")
+            return
+        
+        if not self.source.filter(self.parse):
+            logger.error("Link Error: filter -> parse")
+            return
+        
+        if not self.source.parse(self.convert):
+            logger.error("Link Error: parse -> convert")
+            return
+        
+        if not self.source.convert(self.queue):
+            logger.error("Link Error: convert -> queue")
+            return
+        
+        if not self.source.queue(self.sink):
+            logger.error("Link Error: queue -> sink")
+            return
+        
+class VideoElement(BaseElement):
     def __init__(self, file):
-        self.source = Gst.ElementFactory.make("videotestsrc")
+        super().__init__()
         self.source = Gst.ElementFactory.make("filesrc")
         self.source.set_property('location', file)
         self.decode = Gst.ElementFactory.make("decodebin")
@@ -96,11 +162,6 @@ class VideoElement:
         decode = pad.get_parent()
         pipeline = decode.get_parent()
         decode.link(self.sink)
-
-    def addDrmInfo(self, fd, plane, conn):
-        self.sink.set_property("fd", fd)
-        self.sink.set_property("connector-id", conn.id)
-        self.sink.set_property("plane-id", plane.id)
         
     def addToPipeline(self, container):
         container.pipeline.add(self.source)
@@ -115,9 +176,11 @@ class VideoElement:
 def main():
     vmOne = VMOneContainer()
     videoElement0 = VideoElement("./videos/BlenderReel_1080p.mp4")
-    videoElement1 = VideoElement("./videos/BlenderReel_1080p.mp4")
-    vmOne.addVideoElement0(videoElement0)
-    vmOne.addVideoElement1(videoElement1)
+    #videoElement1 = VideoElement("./videos/BlenderReel_1080p.mp4")
+    hdmiElement = HdmiElement()
+    vmOne.addElement0(videoElement0)
+    #vmOne.addElement1(videoElement1)
+    vmOne.addElement1(hdmiElement)
     vmOne.start()
 
 main()
