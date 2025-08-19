4496ad33c6938f7fa8d47e7614bfe33a5f12c277
zwodev <zwodev@gmail.com>
2025-08-19 15:37:34 +0200

    Added AV_PIX_FMT_YUVJ420P to valid pixel formats for videos.

--- vm1/cpp/source/VideoPlayer.cpp
+++ vm1/cpp/source/VideoPlayer.cpp
@@ -90,6 +90,7 @@ bool VideoPlayer::openFile(const std::string& fileName, AudioStream* audioStream
             codecpar->height == 1080 &&
             codecpar->codec_id == AV_CODEC_ID_HEVC &&
             codecpar->format == AV_PIX_FMT_YUV420P &&
+            codecpar->format == AV_PIX_FMT_YUVJ420P &&
             codecpar->color_space != AVCOL_SPC_BT2020_NCL &&
             codecpar->color_space != AVCOL_SPC_BT2020_CL
         )
