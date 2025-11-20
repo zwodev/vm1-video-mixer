05a5cd79c1206a256b069cf90b432d6d24f97e15
zwodev <zwodev@gmail.com>
2025-11-20 16:29:43 +0100

    Changed version number.

--- vm1/cpp/source/VM1DeviceDefinitions.h
+++ vm1/cpp/source/VM1DeviceDefinitions.h
@@ -2,7 +2,7 @@
 
 #include <string>
 
-const std::string VERSION = "1.0.1";
+const std::string VERSION = "1.1.1";
 const int DISPLAY_WIDTH = 128;
 const int DISPLAY_HEIGHT = 128;
 const int EDIT_BUTTON_COUNT = 8;
