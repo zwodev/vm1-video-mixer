63fb1cf2334c3dd65cb5f00503f971b6f8caa332
zwodev <zwodev@gmail.com>
2026-04-07 18:53:08 +0200

    Added DirectoryCache (WIP).

---
+++ vm1/cpp/source/DirectoryCache.cpp
@@ -0,0 +1,110 @@
+/*
+ * Copyright (c) 2025 Nils Zweiling
+ *
+ * This file is part of VM-1 which is released under the MIT license.
+ * See file LICENSE or go to https://github.com/zwodev/vm1-video-mixer/tree/master/LICENSE
+ * for full license details.
+ */
+
+#include "DirectoryCache.h"
+
+DirectoryCache::DirectoryCache(size_t maxEntries, std::chrono::seconds ttl)
+    : m_maxEntries(maxEntries), m_ttl(ttl) 
+{
+}
+
+std::vector<DirectoryEntry> DirectoryCache::getEntries(const std::string& path) {
+    std::unique_lock lock(m_mutex);
+    MapEntry& entry = m_map[path];
+    if (!entry.node) {
+        entry.node = std::make_shared<DirectoryNode>();
+        entry.lruIt = m_lru.insert(m_lru.begin(), path);
+        enforceCapacity();
+    } else {
+        touchLRU(entry.lruIt);
+    }
+    auto node = entry.node;
+    if (!node->loading.load() || isStale(node)) startAsyncLoad(path, node);
+    return node->entries;
+}
+
+void DirectoryCache::ensureLoaded(const std::string& path) {
+    std::unique_lock lock(m_mutex);
+    MapEntry& entry = m_map[path];
+    if (!entry.node) {
+        entry.node = std::make_shared<DirectoryNode>();
+        entry.lruIt = m_lru.insert(m_lru.begin(), path);
+        enforceCapacity();
+    } else {
+        touchLRU(entry.lruIt);
+    }
+    auto node = entry.node;
+    if (!node->loading.load()) startAsyncLoad(path, node);
+}
+
+void DirectoryCache::invalidate(const std::string& path) {
+    std::unique_lock lock(m_mutex);
+    auto it = m_map.find(path);
+    if (it != m_map.end()) {
+        it->second.node->loadedAt = Clock::time_point{};
+        it->second.node->version++;
+    }
+}
+
+bool DirectoryCache::isStale(const std::shared_ptr<DirectoryNode>& node) const {
+    if (!node->loadedAt.time_since_epoch().count()) return true;
+    return Clock::now() - node->loadedAt > m_ttl;
+}
+
+void DirectoryCache::touchLRU(std::list<std::string>::iterator it) {
+    m_lru.splice(m_lru.begin(), m_lru, it);
+}
+
+void DirectoryCache::enforceCapacity() {
+    while (m_map.size() > m_maxEntries) {
+        auto last = m_lru.end(); --last;
+        m_map.erase(*last);
+        m_lru.erase(last);
+    }
+}
+
+void DirectoryCache::startLoadIfNeeded(const std::string& path, std::shared_ptr<DirectoryNode> node) {
+    if (!node->loading.load()) {
+        startAsyncLoad(path, node);
+    }
+}
+
+void DirectoryCache::startAsyncLoad(const std::string& path, std::shared_ptr<DirectoryNode> node) {
+    node->loading.store(true);
+
+    // Load directory content in separate thread
+    std::weak_ptr<DirectoryNode> weakNode = node;
+    std::string p = path;
+    m_future = std::async(std::launch::async, [this, p, weakNode]() {
+        std::vector<DirectoryEntry> entries;
+        try {
+            for (auto &it : fs::directory_iterator(p)) {
+                DirectoryEntry entry;
+                entry.name = it.path().filename().string();
+                entry.isDir = it.is_directory();
+                if (!entry.isDir) {
+                    std::error_code errorCode;
+                    entry.size = fs::file_size(it.path(), errorCode);
+                    auto ftime = fs::last_write_time(it.path(), errorCode);
+                    if (!errorCode) {
+                        entry.mtime = std::chrono::duration_cast<std::chrono::seconds>(ftime.time_since_epoch()).count();
+                    }
+                }
+                entries.push_back(std::move(entry));
+            }
+        } catch (...) {
+            // No entry if error occurs.
+        }
+        if (auto node = weakNode.lock()) {
+            node->entries = std::move(entries);
+            node->loadedAt = Clock::now();
+            node->loading.store(false);
+            node->version++;
+        }
+    });
+}
---
+++ vm1/cpp/source/DirectoryCache.h
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) 2025 Nils Zweiling
+ *
+ * This file is part of VM-1 which is released under the MIT license.
+ * See file LICENSE or go to https://github.com/zwodev/vm1-video-mixer/tree/master/LICENSE
+ * for full license details.
+ */
+
+#include <filesystem>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <list>
+#include <shared_mutex>
+#include <chrono>
+#include <future>
+#include <atomic>
+#include <optional>
+
+namespace fs = std::filesystem;
+using Clock = std::chrono::steady_clock;
+
+struct DirectoryEntry {
+    std::string name;
+    bool isDir = false;
+    uint64_t size = 0;
+    std::uint64_t mtime = 0;
+};
+
+struct DirectoryNode {
+    bool isValid = false;
+    std::vector<DirectoryEntry> entries;
+    Clock::time_point loadedAt{};
+    std::atomic<bool> loading{false};
+    uint64_t version = 0;
+};
+
+class DirectoryCache {
+public:
+    DirectoryCache(size_t maxEntries = 256, std::chrono::seconds ttl = std::chrono::seconds(5));
+
+    std::vector<DirectoryEntry> getEntries(const std::string& path);
+    void ensureLoaded(const std::string& path);
+    void invalidate(const std::string& path);
+    bool isStale(const std::shared_ptr<DirectoryNode>& node) const;
+
+private:
+    void touchLRU(std::list<std::string>::iterator it);
+    void enforceCapacity();
+    void startLoadIfNeeded(const std::string& path, std::shared_ptr<DirectoryNode> node);
+    void startAsyncLoad(const std::string& path, std::shared_ptr<DirectoryNode> node);
+
+private:
+    struct MapEntry {
+        std::shared_ptr<DirectoryNode> node;
+        std::list<std::string>::iterator lruIt;
+    };
+
+    std::unordered_map<std::string, MapEntry> m_map;
+    std::list<std::string> m_lru;
+    size_t m_maxEntries;
+    std::chrono::seconds m_ttl;
+    mutable std::shared_mutex m_mutex;
+    std::future<void> m_future;
+};
